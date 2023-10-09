5f8db1388df64526f94740392371590a4a40feaa
karo4karo <karolis.sabeckis@stud.kitm.lt>
2023-10-09 10:44:12 +0300

    changed for z6

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# karo
+#!/usr/bin/env python3
+import subprocess
+
+# Replace these with your Zabbix server IP, port, hostname, item key, and new severity
+zabbix_server = '10.75.20.153'
+zabbix_port = '10051'
+host_name = '	snmp_trap_catcher'
+item_key = 'snmptrap[]'
+new_severity = '4'  # 0 - Not classified, 1 - Information, 2 - Warning, 3 - Average, 4 - High, 5 - Disaster
+
+# Construct the zabbix_sender command
+command = [
+    'zabbix_sender',
+    '-z', zabbix_server,
+    '-p', zabbix_port,
+    '-s', host_name,
+    '-k', item_key,
+    '-o', new_severity
+]
+
+try:
+    # Execute the zabbix_sender command
+    subprocess.run(command, check=True)
+    print("Trigger severity updated to " + new_severity + ".")
+except subprocess.CalledProcessError as e:
+    print("Error: " + str(e))
--- zabi2.py
+++ zabi2.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 from pyzabbix import ZabbixAPI
-from pyzabbix import ZabbixSender
 import datetime
 
 def connect_to_zabbix():
     try:
-        z = ZabbixAPI("http://10.75.20.153/zabbix/")
-        z.login(user='Admin', password='zabbix')
+        z = ZabbixAPI("http://10.75.20.28/zabbix/")
+        z.login(api_token='bcf7a642e2dc19f13c502b1447588b570ff47d67b2d301d98377e7d983aaf193')
         return z
     except Exception as e:
         return None
@@ -17,15 +16,6 @@ def jei_none():
     nhour = now.hour
     nminute = now.minute
     return now.hour * now.minute
-
-def send_highest_number():
-    try:
-        max_value = (max(highest_number))
-        zbx = ZabbixSender(zabbix_server='10.75.20.153',zabbix_port=10051)
-        zbx.send(host='oracle_agent_1', key='highest.number', value=max_value)
-        return int(zbx)
-    except Exception as e:
-        return None
 
 def main():
     zabbix_api = connect_to_zabbix()
@@ -38,20 +28,16 @@ def main():
                 x = int(x_input)
 
             count = 1
-            global highest_number
-            highest_number = []
+
             while x != 1:
                 if x % 2 == 0:
                     x = x // 2
                 else:
                     x = 3 * x + 1
-                    highest_number.append(x)
                 count += 1
 
             print(count)
 
-            send_highest_number()
-            
         except ValueError:
             print("Invalid input.")
         except EOFError:
@@ -63,15 +49,13 @@ def main():
                 else:
                     x = 3 * x + 1
                 count += 1
-                
             print(count)
-            
+
         # Logout from the Zabbix API
         try:
             zabbix_api.user.logout()
         except Exception:
             pass  # Ignore logout errors
-
 
 if __name__ == "__main__":
     main()
